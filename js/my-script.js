dcdc8c8a6151b574982c0d568196a0a9b79956be
FrancescoBonandin <bonandinfrancesco@gmail.com>
2023-06-19 14:11:48 +0200

    creato ciclo while di stampa in pagina

--- js/my-script.js
+++ js/my-script.js
@@ -11,3 +11,15 @@ container.append(sLContainer);
 
 sLContainer.classList.add("shopping-list-container");
 
+let index=0;
+
+while(index < shoppingList.length){
+
+    let newLi = document.createElement("li");
+
+    sLContainer.append(newLi);
+    newLi.innerHTML = shoppingList[index];
+
+    index++;
+
+}
