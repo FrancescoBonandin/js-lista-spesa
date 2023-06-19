648ffdf9511fba101a77bc00233a773b08315442
FrancescoBonandin <bonandinfrancesco@gmail.com>
2023-06-19 14:10:36 +0200

    creata ul e appesa

--- js/my-script.js
+++ js/my-script.js
@@ -2,3 +2,12 @@
 
 let shoppingList = ["pane", "latte", "caffè", "acqua", "biscotti"];
 
+
+const container = document.querySelector(".container");
+
+let sLContainer = document.createElement("ul");
+
+container.append(sLContainer);
+
+sLContainer.classList.add("shopping-list-container");
+
